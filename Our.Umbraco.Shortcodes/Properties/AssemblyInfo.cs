de36d1080746a1f3c2a61f459f5d942a58234998
vertino <none@none>
2012-02-19 00:07:04 +0000

    Removed external assembly (Microsoft.Web.Infrastructure.dll), now using NuGet reference.

--- Our.Umbraco.Shortcodes/Properties/AssemblyInfo.cs
+++ Our.Umbraco.Shortcodes/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using Our.Umbraco.Shortcodes.Events;
 
 // Version information for an assembly consists of the following four values:
 // [Major].[Minor].[Build].[Revision]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.*")]
+//[assembly: AssemblyFileVersion("1.2.1.*")]
 
 [assembly: PreApplicationStartMethod(typeof(ApplicationEventsHandler), "RegisterModules")]
